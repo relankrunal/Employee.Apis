8e7ba208233806ee0240e935afc396ae63b5e043
kunalrelan <relan.krunal@gmail.com>
2023-09-04 22:34:52 +0530

    Code for Generic Repository
    Adding code for generic repositry

---
+++ Employee.Data.EF/QueryableExtensions.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Identity.Client;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Employee.Data.EF
+{
+    public static class QueryableExtensions
+    {
+        private const string Ascending = "ASC";
+
+        public static IOrderedQueryable<T> OrderBy<T>(this IQueryable<T> source, string property, string direction)
+        {
+            return direction == Ascending ? source.OrderBy(property) : source.OrderByDescending(property);
+        }
+
+        public static IOrderedQueryable<T> OrderBy<T>(this IQueryable<T> source, string property)
+        {
+            return ApplyOrder(source, property, "OrderBy");
+        }
+
+        public static IOrderedQueryable<T> OrderByDescending<T>(this IQueryable<T> source, string property)
+        {
+            return ApplyOrder(source, property, "OrderByDescending");
+        }
+
+        public static IOrderedQueryable<T> ThenBy<T>(this IOrderedQueryable<T> source, string property, string direction)
+        {
+            return direction == Ascending ? source.ThenBy(property) : source.ThenByDescending(property);
+        }
+
+        public static IOrderedQueryable<T> ThenBy<T>(this IOrderedQueryable<T> source, string property)
+        {
+            return ApplyOrder(source, property, "ThenBy");
+        }
+
+        public static IOrderedQueryable<T> ThenByDescending<T>(this IOrderedQueryable<T> source, string property)
+        {
+            return ApplyOrder(source, property, "ThenByDescending");
+        }
+
+        public static IQueryable<T> IncludePropertyListCsv<T>(this IQueryable<T> query, string includeProperties) where T : class
+        {
+            if (includeProperties != null)
+            {
+                foreach (var item in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(item);
+                }
+            }
+            return query;
+        }
+        private static IOrderedQueryable<T> ApplyOrder<T>(IQueryable<T> source, string property, string methodName)
+        {
+            string[] props = property.Split('.');
+            Type type = typeof(T);
+            ParameterExpression parameter = Expression.Parameter(type, "x");
+
+            Expression expression = parameter;
+
+            foreach (var item in props)
+            {
+                PropertyInfo propertyInfo = type.GetProperty(item, BindingFlags.Instance | BindingFlags.Public | BindingFlags.IgnoreCase);
+                expression = Expression.Property(expression, propertyInfo);
+                type = propertyInfo.PropertyType;
+            }
+
+            Type delegateType = typeof(Func<,>).MakeGenericType(typeof(T), type);
+
+            LambdaExpression lambda = Expression.Lambda(delegateType, expression, parameter);
+
+            object result = typeof(Queryable).GetMethods().Single(
+                m => m.Name == methodName
+                     && m.IsGenericMethodDefinition
+                     && m.GetGenericArguments().Length == 2
+                     && m.GetParameters().Length == 2)
+                .MakeGenericMethod(typeof(T),type)
+                .Invoke(null, new object[] { source, lambda});
+
+            return (IOrderedQueryable<T>)result;
+        }
+    }
+}
+
--- Employee.Repositories.EF/GenericRepository.cs
+++ Employee.Repositories.EF/GenericRepository.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Text;
+using Employee.Data.EF;
 using Employee.Models.Data.Enumerations;
 using Employee.Models.Data.Messages.Response;
 using Employee.Repositories.Interfaces;
@@ -7,60 +10,162 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Employee.Repositories.EF
 {
-	public class GenericRepository<T> : IGenericRepository<T> where T : class
-	{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
         internal AppDbContext context;
         internal DbSet<T> dbSet;
 
-		public GenericRepository(AppDbContext context)
-		{
+        public GenericRepository(AppDbContext context)
+        {
             this.context = context;
             dbSet = context.Set<T>();
-		}
-
-        public Task<PagingList<T>> ExecuteStoreProcedure<I>(string procName, I input, string parameters)
-        {
-            throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<T>> Get(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null)
+        public async Task<PagingList<T>> ExecuteStoreProcedure<I>(string procName, I input, string output)
         {
-            throw new NotImplementedException();
+            var toRetun = new PagingList<T>();
+            var parameters = Parameters.Transform(input, output);
+            var inputParams = parameters.Where(a => a.Direction != System.Data.ParameterDirection.Output)
+                                        .Select(o => o.ParameterName).ToList();
+            var stringOfParameters = string.Empty;
+            var sqlString = new StringBuilder();
+            foreach (var parameter in inputParams)
+            {
+                sqlString.Append($"{parameter} = {parameter}");
+            }
+
+            stringOfParameters = sqlString.ToString();
+
+            if (!string.IsNullOrEmpty(output))
+            {
+                stringOfParameters += $" @{output}";
+            }
+            else
+            {
+                stringOfParameters = stringOfParameters.Remove(stringOfParameters.LastIndexOf(','));
+            }
+
+            var query = await dbSet.FromSqlRaw($"exec {procName} {stringOfParameters}", parameters.ToArray()).ToListAsync();
+            toRetun.AddRange(query);
+
+            if (!string.IsNullOrEmpty(output))
+            {
+
+                toRetun.TotalCount = (int)parameters[parameters.Length - 1].Value;
+            }
+
+            return toRetun;
+        }
+
+        public virtual Task<IEnumerable<T>> Get(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            var results = query.ToList();
+
+            return Task.FromResult<IEnumerable<T>>(results);
         }
 
         public Task<T> GetById(object id)
         {
-            throw new NotImplementedException();
+            var item = dbSet.Find(id);
+            return Task.FromResult(item);
         }
 
         public IQueryable<T> GetQyery(Expression<Func<T, bool>> filter = null, bool IsInclude = false)
         {
-            throw new NotImplementedException();
+            IQueryable<T> query = dbSet;
+
+            if (IsInclude)
+            {
+                var navigationProperties = context.Model.FindEntityType(typeof(T)).GetNavigations();
+
+                foreach (var item in navigationProperties)
+                {
+                    query = query.Include(item.Name);
+                }
+            }
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return query;
         }
 
         public Task<T> Insert(T entity)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(dbSet.Add(entity).Entity);
         }
 
         public Task<List<T>> InsertRage(List<T> entitiesToInsert)
         {
-            throw new NotImplementedException();
+            dbSet.AddRange(entitiesToInsert);
+
+            return Task.FromResult(entitiesToInsert);
         }
 
-        public Task<PagingList<T>> Search(Expression<Func<T, bool>> flter = null, int? pageSize = null, int pageNumber = 1, SortDirection sortDirection = SortDirection.Ascending, string sortField = "", bool isInclude = false)
+        public async Task<PagingList<T>> Search(Expression<Func<T, bool>> filter = null, int? pageSize = null, int pageNumber = 1,
+            SortDirection sortDirection = SortDirection.Ascending, string sortField = "", bool isInclude = false)
         {
-            throw new NotImplementedException();
+            var toRetun = new PagingList<T>();
+
+            IQueryable<T> query = dbSet;
+
+            if (isInclude)
+            {
+                var navigationProperties = context.Model.FindEntityType(typeof(T)).GetNavigations();
+
+                foreach (var item in navigationProperties)
+                {
+                    query = query.Include(item.Name);
+                }
+            }
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (!string.IsNullOrEmpty(sortField))
+            {
+                if (sortDirection == SortDirection.Ascending)
+                {
+                    query = query.OrderBy(property: sortField);
+                }
+
+                if (sortDirection == SortDirection.Descending)
+                {
+                    query = query.OrderByDescending(property: sortField);
+                }
+            }
+
+            toRetun.TotalCount = query.Count();
+
+            if (pageSize != null)
+            {
+                query = query.Skip((pageNumber - 1) * pageSize ?? 0)
+                             .Take(pageSize ?? 0);
+            }
+            var result = await query.ToListAsync();
+            toRetun.AddRange(result);
+            return toRetun;
         }
 
         public Task<T> Update(T entity)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(dbSet.Update(entity).Entity);
         }
 
         public Task<List<T>> UpdateRage(List<T> entitiesToUpdate)
         {
-            throw new NotImplementedException();
+            dbSet.AddRange(entitiesToUpdate);
+
+            return Task.FromResult(entitiesToUpdate);
         }
     }
 }
