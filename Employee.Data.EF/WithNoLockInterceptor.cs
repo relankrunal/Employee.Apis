7bad9635e63bba231046a494c202a991bc5ac8a5
kunalrelan <relan.krunal@gmail.com>
2023-09-04 22:46:02 +0530

    Adding unit of work

---
+++ Employee.Data.EF/WithNoLockInterceptor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Employee.Data.EF
+{
+	public class WithNoLockInterceptor
+	{
+		public WithNoLockInterceptor()
+		{
+		}
+	}
+}
+
--- Employee.Repositories.EF/UnitOfWork.cs
+++ Employee.Repositories.EF/UnitOfWork.cs
@@ -1,11 +1,52 @@
 ﻿using System;
+using Employee.Data.EF;
+using Employee.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace Employee.Repositories.EF
 {
-	public class UnitOfWork
-	{
-		public UnitOfWork()
-		{
-		}
-	}
+    public class UnitOfWork : IUnitOfwork
+    {
+        private AppDbContext context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<int?> ExecuteStoreProcedure<I>(string procName, I input, string output = "", bool forJob = false)
+        {
+            var sqlParams = Parameters.Transform(input, output);
+            var stringOfParameters = string.Join(separator: ",",
+                values: sqlParams.Where(a => a.Direction != System.Data.ParameterDirection.Output).Select(s => s.ParameterName));
+
+            if (!string.IsNullOrEmpty(output))
+            {
+                stringOfParameters += ", @" + output;
+            }
+
+            if (forJob)
+                context.Database.SetCommandTimeout(0);
+
+            var response = await context.Database.ExecuteSqlRawAsync(procName + " " + stringOfParameters, sqlParams);
+            var outParameter = sqlParams.Where(a => a.Direction == System.Data.ParameterDirection.Output).FirstOrDefault();
+            if (outParameter != null)
+            {
+                return (int)outParameter.Value;
+            }
+
+            return null;
+        }
+
+        public IGenericRepository<T> GetRepository<T>() where T : class
+        {
+            return new GenericRepository<T>(context);
+        }
+
+        public void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+    }
 }
 
