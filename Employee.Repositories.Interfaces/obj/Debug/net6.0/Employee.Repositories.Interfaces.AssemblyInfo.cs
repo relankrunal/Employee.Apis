ebf7cadcb3a0e22d1aa1f7f3be393a0d1358aa2d
kunalrelan <relan.krunal@gmail.com>
2024-04-08 11:30:36 -0400

    WIP

--- Employee.Repositories.Interfaces/obj/Debug/net6.0/Employee.Repositories.Interfaces.AssemblyInfo.cs
+++ Employee.Repositories.Interfaces/obj/Debug/net6.0/Employee.Repositories.Interfaces.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee.Repositories.Interfaces")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9de626a211b43d0383c6825abac61b53fbfc92fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee.Repositories.Interfaces")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee.Repositories.Interfaces")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
